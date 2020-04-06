0b9274d17013fec5257462b8ea075e117060fbf5
Nguyen Ngoc Son <sonnn0811@gmail.com>
2020-04-06 10:41:54 +0000

    fix mailer

--- app/mailers/erp/quick_orders/quick_order_mailer.rb
+++ app/mailers/erp/quick_orders/quick_order_mailer.rb
@@ -4,15 +4,15 @@ module Erp::QuickOrders
     helper Erp::OnlineStore::ApplicationHelper
     
     def sending_admin_email_order_confirmation(quick_order)
-      @recipients = ['Hùng Nguyễn <hungnt@hoangkhang.com.vn>', 'Luân Phạm <luanpm@hoangkhang.com.vn>', 'Sơn Nguyễn <sonnn@hoangkhang.com.vn>']
+      @recipients = ['Kinh Doanh <kinhdoanh@hoangkhang.com.vn>', 'Trương Thị Thanh Huyền <huyenttt@hoangkhang.com.vn>', 'Đỗ Thị Đa Nguyên <nguyendtd@hoangkhang.com.vn>', 'Sơn Nguyễn <sonnn@hoangkhang.com.vn>']
       
       @quick_order = quick_order
-      send_email(@recipients.join("; "), "[THCN] -#{Time.current.strftime('%Y%m%d')}- YÊU CẦU ĐẶT HÀNG TRÊN TÌM HÀNG CÔNG NGHỆ")
+      send_email(@recipients.join("; "), "[Tìm Hàng Công Nghệ] - Đơn Đặt Đặt Hàng#{Time.current.strftime('%Y%m%d')}")
     end
     
     def sending_customer_email_order_confirmation(quick_order)
       @quick_order = quick_order
-      send_email(@quick_order.email, "#{Time.current.strftime('%Y%m%d')} - XÁC NHẬN ĐƠN ĐẶT HÀNG")
+      send_email(@quick_order.email, "[Tìm Hàng Công Nghệ] - Xác Nhận Đơn Hàng #{Time.current.strftime('%Y%m%d')}")
     end
   end
 end
